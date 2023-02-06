8e481296734ed590ae199c3c62b5cea73e03e108
ale-rt <alessandro.pisa@gmail.com>
2023-02-06 18:30:42 +0100

    Refactoring

--- src/collective/pintarito/browser/selection.py
+++ src/collective/pintarito/browser/selection.py
@@ -3,6 +3,11 @@ from plone.app.layout.viewlets import ViewletBase
 
 
 class SelectionViewlet(ViewletBase):
+
+    @property
+    def available(self):
+        return self.selection != "default"
+
     @property
     def selection(self):
         return api.portal.get_registry_record(
